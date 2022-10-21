667526d0a95cfff299f31edb81f953026df464a1
Stanghi <stangherlin.matti@gmail.com>
2022-10-21 16:02:26 +0200

    [add] bomb extractor and placement in grid

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 const container = document.querySelector ('.container');
 let elementsPerRow;
+let arrayNumber = [];
+let bombs = [];
 
 const btn = document.getElementById('play');
 btn.addEventListener('click', function(){
@@ -9,6 +11,8 @@ btn.addEventListener('click', function(){
     document.getElementById('play')
     container.innerHTML = '';
     init(elementsPerRow);
+    createBomb();
+    arrayNumber = [];
 });
 
 function difficulty(difficultyChosen){
@@ -28,7 +32,9 @@ function init(numElements){
 
     for (let i = 0; i < totalSqares; i++){
         createSquare(i);
+        arrayNumber.push(i + 1);
     }
+    console.log(arrayNumber);
 }
 
 function createSquare(maxSquares){
@@ -50,4 +56,17 @@ function clickSquare(){
     console.log(this.idElement);
     this.innerHTML = this.idElement;
     this.classList.add('square-colored');
+
+    if (bombs.includes(this.idElement)){
+        console.log("GAME OVER");
+        this.classList.add('bomb');
+    }
+}
+
+function createBomb(){
+        // Shuffle arrayNumber
+        const shuffled = arrayNumber.sort(() => 0.5 - Math.random());
+        // Get sub-arrayNumber of first n elements after shuffled
+        bombs = shuffled.slice(0, 16);
+        console.log("BOMBE",bombs);
 }
